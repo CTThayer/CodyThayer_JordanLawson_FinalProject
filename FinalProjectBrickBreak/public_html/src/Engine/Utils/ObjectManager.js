b55c87396cede17b8fa71ce2196686eb4f034cef
CTThayer <44277556+CTThayer@users.noreply.github.com>
2020-03-15 19:41:24 -0700

    ObjectManager consolidations + line debug added
    ObjectManager consolidations + line debug added

--- FinalProjectBrickBreak/public_html/src/Engine/Utils/ObjectManager.js
+++ FinalProjectBrickBreak/public_html/src/Engine/Utils/ObjectManager.js
@@ -10,10 +10,11 @@ function ObjectManager(objectArray, texture) {
     this.objectArray = objectArray;
     this.texture = texture;
     this.quadMode = false;
-    this.quadTree = null;
+    this.quadTree = new Quadtree([-100, 100, -75, 75], 8, 10);
     this.visualization = false;
-}
-;
+    this.borderLinesActive = false;
+    this.borderLines = null;
+};
 
 ObjectManager.prototype.addObject = function (object) {
     this.objectArray.push(object);
@@ -28,15 +29,12 @@ ObjectManager.prototype.draw = function (camera) {
     for (var i = 0; i < this.objectArray.length; i++) {
         this.objectArray[i].draw(camera);
     }
-};
-
-ObjectManager.prototype.quadTreeMode = function () {
-    this.quadMode = !this.quadMode;
-
-    if (this.quadTree === null) {
-        this.quadTree = new Quadtree([-100, 100, -75, 75], 4, 10);
-    } else
-        this.quadTree = null;
+    
+    if (this.borderLinesActive) {
+        for (var j = 0; j < this.borderLines.length; j++) {
+            this.borderLines[j].draw(camera);
+        }
+    }
 };
 
 ObjectManager.prototype.updateTree = function () {
@@ -56,8 +54,8 @@ ObjectManager.prototype.collisionCheck = function () {
             var h = [];
 
             if (this.objectArray[i].pixelTouches(this.objectArray[j], h)) {
-                this.objectArray[i].mDyePack.setColor([1, 0, 0, 1]);
-                this.objectArray[j].mDyePack.setColor([1, 0, 0, 1]);
+                this.objectArray[i].setColor([1, 0, 0, 1]);
+                this.objectArray[j].setColor([1, 0, 0, 1]);
             }
         }
     }
@@ -70,7 +68,7 @@ ObjectManager.prototype.quadCollisionCheck = function () {
         for (var j = 0; j < collisionArray.length; j++) {
             var h = [];
             if (this.objectArray[i] !== collisionArray[j] && this.objectArray[i].pixelTouches(collisionArray[j], h)) {
-                collisionArray[j].mDyePack.setColor([1, 0, 0, 1]);
+                collisionArray[j].setColor([1, 0, 0, 1]);
             }
         }
     }
@@ -79,57 +77,54 @@ ObjectManager.prototype.quadCollisionCheck = function () {
 ObjectManager.prototype.update = function () {
 
     // Create more DyePacks
-    if (gEngine.Input.isKeyPressed(gEngine.Input.keys.Space)) {
-        var dyePack = new DyePack(this.texture);
-        this.objectArray.push(dyePack);
+    if (gEngine.Input.isKeyClicked(gEngine.Input.keys.Space)) {
+        for (var i = 0; i < 10; i++) {
+            var dyePack = new DyePack(this.texture);
+            this.objectArray.push(dyePack);
+        }
     }
 
-    // Change to QuadTree
+    // Change to QuadTree Mode
     if (gEngine.Input.isKeyClicked(gEngine.Input.keys.Q)) {
         this.quadMode = !this.quadMode;
-
-        if (this.quadTree === null) {
-            this.quadTree = new Quadtree([-100, 100, -75, 75], 4, 10);
-            this.updateTree();
-        } else {
-            this.quadTree = null;
-            this.visualization = false;
-        }
     }
 
     // Turn On QuadTree Visualization
     if (gEngine.Input.isKeyClicked(gEngine.Input.keys.Z)) {
-
-        // Make sure that mQuadTree is on
-        if (this.quadTree === null) {
-            this.quadMode = !this.quadMode;
-            this.quadTree = new Quadtree([-100, 100, -75, 75], 4, 10);
-        }
         this.visualization = !this.visualization;
     }
+    
+    if (gEngine.Input.isKeyClicked(gEngine.Input.keys.L)) {
+        this.borderLinesActive = !this.borderLinesActive;
+    }
 
-
+    // Reset color on all objects and update their positions
     for (var i = 0; i < this.objectArray.length; i++) {
-        this.objectArray[i].mDyePack.setColor([0, 0, 0, 0]);
+        this.objectArray[i].setColor([0, 0, 0, 0]);
         this.objectArray[i].update();
     }
 
-
-
+    // If we are NOT in quadMode run regular brute force collision
+    // If we ARE in quadMode: update tree, run quadtree collision, and run visualization if active
     if (!this.quadMode)
         this.collisionCheck();
     else {
         this.updateTree();
         this.quadCollisionCheck();
-    }
-
-    if (this.visualization) {
-        var quadrant = Array.from(this.quadTree.getObjectsNear(this.objectArray[0]));
-        
-        for (var i = 0; i < quadrant.length; i++) {
-            quadrant[i].mDyePack.setColor([0, 1, 0, 1]);
+        if (this.visualization) {
+            this.runVisualization();
         }
-        
-        this.objectArray[0].mDyePack.setColor([0, 0, 1, 1])
+    }
+};
+
+ObjectManager.prototype.runVisualization = function () {
+    var quadrant = Array.from(this.quadTree.getObjectsNear(this.objectArray[0]));
+    for (var i = 0; i < quadrant.length; i++) {
+        quadrant[i].setColor([0, 1, 0, 1]);
+    }
+    this.objectArray[0].setColor([0, 0, 1, 1]);
+
+    if (this.borderLinesActive) {
+        this.borderLines = this.quadTree.getQuadLines();
     }
 };
